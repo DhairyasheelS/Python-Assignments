4fc5eaa251c7e9c525c50de3e763d7bc804258ad
Dhairyasheel Shinde <shinde.dhairyasheel05@gmail.com>
2026-06-14 14:40:59 +0530

    adding ans of assignment 10

--- Assignment01/Assignment01_10.py
+++ Assignment01/Assignment01_10.py
@@ -5,3 +5,9 @@ print(10 + 20)
 
 # Explain the difference
 
+# The output of the code will be:
+# 1020
+# 30 
+
+# The difference is that "10" and "20" are strings, so the + operator concatenates them.
+# 10 and 20 are integers, so the + operator performs addition.
