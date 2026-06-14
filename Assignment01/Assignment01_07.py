12dc46c16a94edd9bcfa0e3e47ec285cb7e529ec
Dhairyasheel Shinde <shinde.dhairyasheel05@gmail.com>
2026-06-14 14:42:30 +0530

    adding ans of assignment of 07 and 08

---
+++ Assignment01/Assignment01_07.py
@@ -0,0 +1,7 @@
+# Q7 : why does the input() function always return a string
+# How can you convert it to another data type?
+
+# The input() function in Python always returns a string because it is designed to read user input as text. This allows users to enter any type of data, including numbers, without causing errors. 
+# To convert the input to another data type, 
+# you can use type conversion functions such as int(), float(), or bool() depending on the desired data type.
+#For example, if you want to convert the input to an integer, you can use int(input()).
---
+++ Assignment01/Assignment01_08.py
@@ -0,0 +1,8 @@
+# Q8 : Predict the output:
+x = input("Enter Number :")
+print(type(x))
+
+# Explain the output.
+
+# The output of the code will be <class 'str'> regardless of the input provided by the user.
+# This is because the input() function in Python always returns a string.
