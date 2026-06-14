9d82241bb5b7f2941ed5fe7d22ce3f46214c6955
Dhairyasheel Shinde <shinde.dhairyasheel05@gmail.com>
2026-06-14 14:43:59 +0530

    ans of assignment 04

---
+++ Assignment01/Assignment01_04.py
@@ -0,0 +1,11 @@
+# Q4 : what is purpose of getsizeof()?
+# why is memory size different for different data types?
+
+# The getsizeof() function in Python is used to get the size of an object in bytes. 
+# It returns the memory size of the object, 
+# including the overhead of the object itself and 
+# any additional memory used by the object to store its data.
+
+# The memory size is different for different data types because different data types have different memory requirements. 
+# For example, an integer typically requires less memory than a string or a list because it is a simpler data type.
+# Additionally, the memory size of an object can also depend on the implementation of Python and the underlying hardware architecture.
